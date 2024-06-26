285afa4574c8edbaa55045e01ac8d06373674f46
awolcat <albertirura11@gmail.com>
2024-06-26 16:58:11 +0300

    added static root

--- Mekason/Mekason/settings.py
+++ Mekason/Mekason/settings.py
@@ -117,7 +117,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -154,6 +153,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles","static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
