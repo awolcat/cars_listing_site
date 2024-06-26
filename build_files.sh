119aab750a9674a29bf0c04c4efeef771cc2f982
awolcat <albertirura11@gmail.com>
2024-06-26 18:08:09 +0300

    changed python runtime in build script

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,9 @@
 echo "BUILD START"
-python3.9 -m venv venv
+python3 -m venv venv
 
 # activate the virtual environment
 source venv/bin/activate
 
 pip install -r Mekason/requirements.txt
-python3.9 Mekason/manage.py collectstatic --noinput
+python3 Mekason/manage.py collectstatic --noinput
 echo "BUILD END"
