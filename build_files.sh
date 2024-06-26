1e74c0f7236cd1f704e5b5c281d50e61eb8a2f40
awolcat <albertirura11@gmail.com>
2024-06-26 16:06:59 +0300

    added build script

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
+echo "BUILD START"
+python3.9 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+
 pip install -r Mekason/requirements.txt
 python3.9 Mekason/manage.py collectstatic --noinput
+echo "BUILD END"
