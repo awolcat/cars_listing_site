6dce12a26bcaea58ee0828694c687ec07de97e2b
awolcat <albertirura11@gmail.com>
2024-06-28 14:25:03 +0300

    delete env after build

--- build_files.sh
+++ build_files.sh
@@ -6,4 +6,8 @@ source venv/bin/activate
 
 pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
+
+# deactivate the virtual environment and delete it
+deactivate
+rm -rf env
 echo "BUILD END"
