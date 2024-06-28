4b1d8a44991c6ed44f6a4a993e379694845ad07b
awolcat <albertirura11@gmail.com>
2024-06-28 13:19:58 +0300

    vercel config

--- build_files.sh
+++ build_files.sh
@@ -4,6 +4,6 @@ python3 -m venv venv
 # activate the virtual environment
 source venv/bin/activate
 
-pip install -r Mekason/requirements.txt
-python3 Mekason/manage.py collectstatic --noinput
+pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
 echo "BUILD END"
